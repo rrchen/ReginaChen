7b4b188c22fa050f7e34afe6c4e4c17a6639978a
Regina Chen <rchen021@gmail.com>
2016-12-04 23:58:54 -0800

    scores and grades commit

---
+++ Python/fundamentals/scores_grades.py
@@ -0,0 +1,18 @@
+def scoresGrades():
+    print "Scores and Grades"
+    for num in range(10):
+        score = int(raw_input())
+        if (score >= 90):
+            grade = "A"
+        elif (score >= 80):
+            grade = "B"
+        elif (score >= 70):
+            grade = "C"
+        elif (score >= 60):
+            grade = "D"
+        else
+            grade = "FAIL"
+        print "Score: " + str(score) + "; Your grade is " + grade
+    print "End of the program. Bye!"
+
+scoresGrades()
